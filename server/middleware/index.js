75805f67652607e4c5c3be3d2ea44681867b2288
Patrick Nilan <nilan.patrick@gmail.com>
2023-08-01 14:11:01 -0700

    Updated auth error handling

--- server/middleware/index.js
+++ server/middleware/index.js
@@ -16,17 +16,25 @@ module.exports = {
       .then((decodedToken) => {
         req.headers.uid = decodedToken.uid;
         return decodedToken.uid;
-        })
-        .then((uid) => {
-          return db.query('SELECT id FROM users WHERE uid = $1', [uid]);
-        })
-        .then((userId) => {
-          req.headers.userId = userId;
-        })
-        .catch((err) => {
+      })
+      .catch((err) => {
           console.error('Error decoding token:', err);
           res.status(403).send({ error: err });
-        });
+      });
+      .then((uid) => {
+          return db.query('SELECT id FROM users WHERE uid = $1', [uid]);
+      })
+      .then((userId) => {
+          if (!userId) => {
+            res.status(404).end();
+          } else {
+            req.headers.userId = userId;
+          }
+      })
+      .catch((err) => {
+        console.error('Error retrieving user_id from database:', err);
+        res.status(500).end();
+      })
     }
 
   }
