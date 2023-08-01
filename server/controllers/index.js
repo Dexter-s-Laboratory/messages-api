f6b366a7dd34b8a3fe53834780c82b6f6467cdde
Patrick Nilan <nilan.patrick@gmail.com>
2023-08-01 08:52:27 -0700

    Added stubbed controller functions

--- server/controllers/index.js
+++ server/controllers/index.js
@@ -1,2 +1,24 @@
-exports.example = require('./example.js');
-exports.rootJestTest = require('./rootJestTest.js')
+const model = require('../models');
+
+module.exports = {
+
+  getMessages: (req, res) => {
+    res.end();
+  },
+
+  getMessagesByConversationId: (req, res) => {
+    res.end();
+  },
+
+  createMessage: (req, res) => {
+    res.end();
+  },
+
+  archiveConversation: (req, res) => {
+    res.end();
+  },
+
+  deleteMessage: (req, res) => {
+    res.end();
+  }
+};
